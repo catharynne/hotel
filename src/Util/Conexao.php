ff43c3036893594657d068799809326ec2e0752f
iftm <iftm@labXX-mXX.laboratorios>
2018-04-06 22:26:56 -0300

    começado mecher no bacno

--- src/Util/Conexao.php
+++ src/Util/Conexao.php
@@ -11,7 +11,8 @@ class Conexao {
        if(!self::$instancia){
            self::$instancia = new PDO("mysql:host=localhost;dbname=NOME_BANCO",'USUARIO','SENHA');
            self::$instancia->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES uft8");
-           self::$instancia->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES uft8");
+           self::$instancia->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+           self::$instancia->setAttribute(PDO::ATTR_ORACLE_NULLS,PDO::NULL_EMPTY_STRING);
            
            
        }
